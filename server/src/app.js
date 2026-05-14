af9018164d398599498f45b524c9ffee60c58e2c
Ankit-0009 <ankitcode2511@gmail.com>
2026-05-15 01:06:25 +0530

    cros fix

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,10 @@ import signalRoutes from "./routes/signal.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
