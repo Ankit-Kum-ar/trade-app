69e5311889b5ea2f1824c9d9631b1adb9883112d
Ankit-0009 <ankitcode2511@gmail.com>
2026-05-15 01:24:30 +0530

    route fix

--- server/src/routes/signal.routes.js
+++ server/src/routes/signal.routes.js
@@ -11,8 +11,8 @@ const router = express.Router();
 
 router.post("/", createSignal);
 router.get("/", getAllSignals);
+router.get("/:id/status", getSignalStatus);
 router.get("/:id", getSignalById);
 router.delete("/:id", deleteSignal);
-router.get("/:id/status", getSignalStatus);
 
 export default router;
